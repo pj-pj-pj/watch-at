f81d5775f142f91c067cad7a6758fd0a636eeab2
Paula Joyce Ucol <peaksenut@gmail.com>
2024-12-22 00:30:40 +0800

    Project Creation

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace watch_at.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
